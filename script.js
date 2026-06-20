3ad287beb938daf5e38dc1e5cfd4744ad939ac00
Arianna <143475620+arianna-k@users.noreply.github.com>
2026-06-20 01:36:27 -0400

    Calculator Tabs
    Adding the Style for Calculator tabs, as well as the html for it to test if it is how I want it to look.

---
+++ script.js
@@ -0,0 +1,21 @@
+function openTab(event, tabId) {
+
+    // Hide all content
+    const contents = document.querySelectorAll(".tab-content");
+    contents.forEach(content => {
+        content.classList.remove("active-content");
+    });
+
+    // Remove active button
+    const buttons = document.querySelectorAll(".tab-button");
+    buttons.forEach(button => {
+        button.classList.remove("active");
+    });
+
+    // Show selected content
+    document.getElementById(tabId)
+        .classList.add("active-content");
+
+    // Highlight selected button
+    event.currentTarget.classList.add("active");
+}
