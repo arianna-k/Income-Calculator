ffc4d9d490ed1220a370acb65f45e76c261f7b71
Arianna <143475620+arianna-k@users.noreply.github.com>
2026-07-03 00:36:43 -0400

    Updated Calculator toggle format
    Script update to try to fix calculator toggle issue

--- script.js
+++ script.js
@@ -27,3 +27,42 @@ function openTab(event, tabId) {
     // Highlight selected button
     event.currentTarget.classList.add("active");
 }
+
+function toggleYears() {
+
+    const years = document.getElementById("years").value;
+
+    const message = document.getElementById("selectMessage");
+    const year1 = document.getElementById("year1Column");
+    const year2 = document.getElementById("year2Column");
+
+    if (years === "0") {
+
+        message.style.display = "block";
+
+        year1.style.display = "none";
+        year2.style.display = "none";
+
+    }
+    else if (years === "1") {
+
+        message.style.display = "none";
+
+        year1.style.display = "block";
+        year2.style.display = "none";
+
+    }
+    else {
+
+        message.style.display = "none";
+
+        year1.style.display = "block";
+        year2.style.display = "block";
+
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    toggleYears();
+});
